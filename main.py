4a3b00449d2f8098dfe1097250bfd62cec2b566d
Deniz Kacmaz <kacmaz.denizniclas@gmail.com>
2024-03-09 14:09:22 +0100

    quatationg and correcting input

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+# Remove whitespaces from str
+name = name.strip()
+
 # Diffrent ways to say hello to user
 #In one line
 print("hello, " + name)
 print("hello,", name)
-print("hello," , name, sep="")
+print(f"hello, {name}")
 
 #In two lines
 print("hello, ", end="")
 print(name)
+
+#how to use quations
+print('hello, "friend"')
+print("hello, \"friend\"")
